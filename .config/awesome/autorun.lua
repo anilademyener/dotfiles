e59195ecd69e2286b51aa8b3ec41131bd7f9c107
anilademyener <anilademyener@gmail.com>
2019-09-05 15:52:35 +0300

    first commit

---
+++ .config/awesome/autorun.lua
@@ -0,0 +1,33 @@
+#!/usr/bin/env lua
+
+function filecheck(file)
+    local ok, err, code = os.rename(file, file)
+    if not ok then
+        if code == 13 then
+            return true
+        else
+            return false
+        end
+    end
+    return ok, err
+end
+
+function remove()
+    print("# AUTORUN IS REMOVING FROM YOUR SYSTEM")
+    os.execute("sudo rm -rf /usr/local/bin/autorun.lua")
+end
+
+function main()
+    if filecheck("/usr/local/bin/autorun.lua") == true then
+        os.execute("compton --config ~/.config/compton/compton.conf -b")
+    else
+        print("# AUTORUN IS COPYING TO YOUR SYSTEM")
+        os.execute("sudo cp -r autorun.lua /usr/local/bin/autorun.lua")
+    end
+end
+
+if arg[1] == "--remove" then
+    remove()
+else
+    main()
+end
